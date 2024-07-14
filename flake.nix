fbb83133a3325a3750c9a44fbfd7ca4553a80fa1
emctoo <maple.hl@gmail.com>
2024-07-14 22:23:28 +0800

    use nightly

--- flake.nix
+++ flake.nix
@@ -1,76 +1,42 @@
 # https://drakerossman.com/blog/rust-development-on-nixos-bootstrapping-rust-nightly-via-flake
 {
+  description = "A devShell example";
+
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-
-    # crane = {
-    #   url = "github:ipetkov/crane";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
-    systems.url = "github:nix-systems/default";
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    systems,
-    nixpkgs,
-    treefmt-nix,
-    ...
-  } @ inputs: let
-    eachSystem = f:
-      nixpkgs.lib.genAttrs (import systems) (
-        system:
-          f (import nixpkgs {
-            inherit system;
-            overlays = [inputs.rust-overlay.overlays.default];
-          })
-      );
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            eza
+            fd
+            (
+              rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+                extensions = [ "rust-src" ];
+                targets = [ "arm-unknown-linux-gnueabihf" ];
+              })
+            )
+          ];
 
-    rustToolchain = eachSystem (pkgs: pkgs.rust-bin.stable.latest);
-
-    treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
-  in {
-    # You can use crane to build the Rust application with Nix
-
-    # packages = eachSystem (pkgs: let
-    #   craneLib = inputs.crane.lib.${pkgs.system};
-    # in {
-    #   default = craneLib.buildPackage {
-    #     src = craneLib.cleanCargoSource (craneLib.path ./.);
-    #   };
-    # });
-
-    devShells = eachSystem (pkgs: {
-      # Based on a discussion at https://github.com/oxalica/rust-overlay/issues/129
-      default = pkgs.mkShell (with pkgs; {
-        nativeBuildInputs = [
-          clang
-          # Use mold when we are runnning in Linux
-          (lib.optionals stdenv.isLinux mold)
-        ];
-        buildInputs = [
-          rustToolchain.${pkgs.system}.default
-          rust-analyzer-unwrapped
-          cargo
-          # pkg-config
-          # openssl
-        ];
-        RUST_SRC_PATH = "${
-          rustToolchain.${pkgs.system}.rust-src
-        }/lib/rustlib/src/rust/library";
-      });
-    });
-
-    formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
-
-    checks = eachSystem (pkgs: {
-      formatting = treefmtEval.${pkgs.system}.config.build.check self;
-    });
-  };
+          shellHook = ''
+            alias ls=eza
+            alias find=fd
+          '';
+        };
+      }
+    );
 }
